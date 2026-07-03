482d30bca4aac35dc53a9cacd4ceca0a8855bb31
iboozkann1 <smirnovartem239@gmail.com>
2026-07-03 13:55:33 +0300

    Delete nova-service directory

--- nova-service/js/config.js
+++
@@ -1,6 +0,0 @@
-// ===== НАСТРОЙКИ TELEGRAM =====
-const TELEGRAM_TOKEN = '8907128440:AAHgC74ZpcaM8vgq6_UR0-f88NpUjch2BhQ';
-const TELEGRAM_CHAT_ID = '135222037';
-
-// ===== НАСТРОЙКИ ТЕЛЕФОНА =====
-const PHONE_NUMBER = '+79049139157';
